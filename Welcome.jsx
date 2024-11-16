a9a5a39eb09cc7f08f24c7469e7ab9d9b4dfa41c
thordeva <159133164+thordeva@users.noreply.github.com>
2024-11-16 10:23:21 +0530

    Pages

---
+++ Welcome.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { useEffect } from "react";
+
+import Footer from "../componets/Footer/Footer";
+
+import WelcomePageImage1 from "../images/WelcomePageImage1.png";
+import WelcomePageImage2 from "../images/WelcomePageImage2.png";
+import WelcomePageImage3 from "../images/WelcomePageImage3.png";
+import WelcomePageImage4 from "../images/WelcomePageImage4.png";
+import WelcomePageBanner from "../images/WelcomePageBanner.jpg";
+
+import { Fade } from "react-reveal";
+import { Link } from "react-router-dom";
+
+function Welcome() {
+  useEffect(() => {
+    const image1 = WelcomePageImage1;
+  }, []);
+
+  return (
+    <div>
+      <div
+        style={{
+          background: `linear-gradient(0deg, hsl(0deg 0% 0% / 73%) 0%, hsl(0deg 0% 0% / 73%) 35%),url(${WelcomePageBanner})`,
+        }}
+        className="h-[32rem] w-full sm:h-[65vh] xl:h-[80vh] bg-slate-800 relative"
+      >
+        <div className="grid content-center justify-center h-full justify-items-center">
+          <div className="w-10/12 text-center sm:w-11/12 md:w-40rem">
+            <Fade duration={2000}>
+              <h1 className="mb-3 text-3xl font-semibold text-center text-white sm:text-4xl md:text-6xl">
+                Movie Recommendation
+              </h1>
+              <h1 className="mb-4 text-xl text-center text-stone-400 font-light sm:text-2xl"></h1>
+              <h1 className="mb-2 text-center text-stone-400 font-light sm:text-xl sm:mb-8">
+                Ready to watch? Enter your email to create an account.
+              </h1>
+              <div>
+                <input
+                  placeholder="Email Address"
+                  className="w-full p-2 py-3 rounded-sm sm:py-4 md:py-5 md:w-3/4"
+                />
+                <Link to={"/signup"}>
+                  <button className="px-4 py-2 mt-3 font-medium text-white bg-red-700 rounded-sm sm:py-4 md:mt-0 md:pb-5 md:text-xl md:w-1/4">
+                    Get Started
+                  </button>
+                </Link>
+              </div>
+            </Fade>
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundImage:
+              "linear-gradient(hsl(0deg 0% 0% / 0%), hsl(0deg 0% 0% / 38%), hsl(0deg 0% 7%))",
+          }}
+        ></div>
+      </div>
+
+      {/* Section 4 */}
+      <section className="bg-black border-y-8 border-y-zinc-800"></section>
+
+      {/* Section 5 */}
+      <section className="bg-black">
+        <Fade>
+          <div className="flex justify-center">
+            <div className="flex flex-col-reverse lg:flex-row lg:items-center lg:w-9/12">
+              <div className="flex justify-center"></div>
+              <div>
+                <h1 className="mt-4 mb-6 text-4xl font-semibold text-center text-white lg:mt-0 lg:text-left lg:ml-8 lg:text-5xl xl:text-6xl"></h1>
+                <h1 className="m-4 text-center text-stone-400 font-light lg:text-left lg:ml-8 lg:text-2xl lg:w-9/12"></h1>
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+
+      {/* Section 6 */}
+      <section></section>
+
+      {/* Footer */}
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default Welcome;
