56d425f7ee1a430d1dbc6a27db8cbc02aa6818bf
thordeva <159133164+thordeva@users.noreply.github.com>
2024-11-16 10:29:06 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+function Footer2() {
+  return (
+    <div className="bg-black p-2">
+      <footer className={styles.footer}>
+        <div className={styles.containerFooter}>
+          <div className={styles.icons}></div>
+          <ul className={styles.details}>
+            <li>Contact : 8220987501</li>
+            <li></li>
+            <li>Movie Recommendation</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <div>
+            <div></div>
+            <span></span>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer2;
